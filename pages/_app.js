1c471d3525b8f6e8ccccf728321dff4b1b0f068f
Dharaneesh0745 <123287037+Dharaneesh0745@users.noreply.github.com>
2023-04-30 20:56:26 +0530

    Updated App.js file with Header & Footer.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import Head from 'next/head'
+import Footer from '@/layout/Footer'
+import Header from '@/layout/Header'
+import 'styles/globals.css'
+import 'styles/layout.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  if (Component.getLayout) {
+    return Component.getLayout(<Component {...pageProps} />)
+  }
+  return (
+    <>
+      <Head>
+        <title>Codevolution</title>
+        <meta name='description' content='Awesome YouTube channel' />
+      </Head>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  )
 }
 
 export default MyApp
