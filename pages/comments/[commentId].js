e97f3a718bf86da5b16e655d6ffc0142eb2b7f7d
Dharaneesh0745 <123287037+Dharaneesh0745@users.noreply.github.com>
2023-04-30 19:42:23 +0530

    APIs and Pre - rendering.
    Updated file [commentId].js for APIs and Pre - rendering.

--- pages/comments/[commentId].js
+++ pages/comments/[commentId].js
@@ -7,7 +7,87 @@ export default function handler(req, res) {
     res.status(200).json(comment)
   } else if (req.method === 'DELETE') {
     const deletedComment = comments.find(
-      comment => comment.id === parseInt(commentId)
+      comment => comment.id === parseInt(commentId)import { comments } from '../../data/comments'
+
+function Comment({ comment }) {
+  return (
+    <div>
+      {comment.id}. {comment.text}
+    </div>
+  )
+}
+
+export default Comment
+
+export async function getStaticProps(context) {
+  const { params } = context
+  const { commentId } = params
+
+  const comment = comments.find(comment => comment.id === parseInt(commentId))
+  console.log(comment)
+
+  /** Don't do this 
+  const response = await fetch(`http:localhost:3000/api/comments/${commentId}`)
+  const data = await response.json()
+  */
+
+  return {
+    props: {
+      comment
+    }
+  }
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { commentId: '1' } },
+      { params: { commentId: '2' } },
+      { params: { commentId: '3' } }
+    ],
+    fallback: false
+  }
+}import { comments } from '../../data/comments'
+
+function Comment({ comment }) {
+  return (
+    <div>
+      {comment.id}. {comment.text}
+    </div>
+  )
+}
+
+export default Comment
+
+export async function getStaticProps(context) {
+  const { params } = context
+  const { commentId } = params
+
+  const comment = comments.find(comment => comment.id === parseInt(commentId))
+  console.log(comment)
+
+  /** Don't do this 
+  const response = await fetch(`http:localhost:3000/api/comments/${commentId}`)
+  const data = await response.json()
+  */
+
+  return {
+    props: {
+      comment
+    }
+  }
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { commentId: '1' } },
+      { params: { commentId: '2' } },
+      { params: { commentId: '3' } }
+    ],
+    fallback: false
+  }
+}
     )
     const index = comments.findIndex(
       comment => comment.id === parseInt(commentId)
