3249795138f0f197ddc8254064ec4017b1064fa0
Dharaneesh0745 <123287037+Dharaneesh0745@users.noreply.github.com>
2023-04-30 12:16:47 +0530

    Sucessfully runned static generation in next.js.
    Clicking users link in index page and displaying JSON data in next page. That JSON fetching is already done in users.js in the components folder.

--- pages/pages/index.js
+++ pages/pages/index.js
@@ -1,5 +1,12 @@
+import Link from 'next/link'
+
 function Home() {
-  return <h1>Next JS pre-rendering</h1>
+  return (
+    <>
+      <h1>Next JS pre-rendering</h1>
+      <Link href='/users'>Users</Link>
+    </>
+  )
 }
 
 export default Home
