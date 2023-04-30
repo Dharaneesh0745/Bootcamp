43a6115b89185d8d8630bc979f1a6d1a27b00e95
Dharaneesh0745 <123287037+Dharaneesh0745@users.noreply.github.com>
2023-04-30 12:58:03 +0530

    SSG with getStaticPaths.
    Got some errors in the previous commit. The error was not defined the static path and the params are not defined. Solved the error in this commit.

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -11,6 +11,23 @@ function Post( { post } ) {
 
 export default Post
 
+export async function getStaticPaths() {
+    return {
+        paths: [
+            {
+                params: { postId: '1' },
+            },
+            {
+                params: { postId: '2' },
+            },
+            {
+                params: { postId: '3' },
+            },
+        ],
+        fallback: false,
+    }
+}
+
 export async function getStaticProps(context) {
     const { params } = context
     const response = await fetch('https://jsonplaceholder.typicode.com/posts/${params.postId}')
