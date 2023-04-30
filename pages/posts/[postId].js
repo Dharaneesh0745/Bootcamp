96ad6dc591a4f01bd67ec23628bf60ac6a30d624
Dharaneesh0745 <123287037+Dharaneesh0745@users.noreply.github.com>
2023-04-30 15:01:09 +0530

    getStaticPaths fallback true.

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -1,4 +1,13 @@
+import { useRouter } from 'next/router'
+
 function Post( { post } ) {
+
+    const router = useRouter()
+
+    if(router.isFallback) {
+        return <h1>Loading......</h1>
+    }
+
     return (
         <>
             <h2>
@@ -13,39 +22,46 @@ export default Post
 
 export async function getStaticPaths() {
 
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts')
-    const data = await response.json()
+    // const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+    // const data = await response.json()
 
-    const paths = data.map((post) => {
-        return {
-            params: {
-                postId: `${post.id}`,
-            }
-        }
-    })
+    // const paths = data.map((post) => {
+    //     return {
+    //         params: {
+    //             postId: `${post.id}`,
+    //         }
+    //     }
+    // })
 
     return {
-        // paths: [
-        //     {
-        //         params: { postId: '1' },
-        //     },
-        //     {
-        //         params: { postId: '2' },
-        //     },
-        //     {
-        //         params: { postId: '3' },
-        //     },
-        // ],
-        paths,
-        fallback: false,
+        paths: [
+            {
+                params: { postId: '1' },
+            },
+            {
+                params: { postId: '2' },
+            },
+            {
+                params: { postId: '3' },
+            },
+        ],
+        fallback: true,
     }
 }
 
 export async function getStaticProps(context) {
     const { params } = context
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts/${params.postId}')
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`)
 
     const data = await response.json()
+
+    if(!data.id) {
+        return {
+            notFound: true,
+        }
+    }
+
+    console.log(`Generating page for /posts/${params.postId}`)
 
     return {
         props: {
