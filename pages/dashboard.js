5190d2f13b719b092852f205973d2f36fac06d56
Dharaneesh0745 <123287037+Dharaneesh0745@users.noreply.github.com>
2023-04-30 17:57:31 +0530

    Client-side Data Fetching.
    Created a new file inside the pages folder named as dashboard.js. And working on Client-Side Data Fetching.

---
+++ pages/dashboard.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react'
+
+function Dashboard() {
+  const [isLoading, setIsLoading] = useState(true)
+  const [dashboardData, setDashboardData] = useState(null)
+  useEffect(() => {
+    async function fetchDashboardData() {
+      const response = await fetch('http://localhost:4000/dashboard')
+      const data = await response.json()
+      setDashboardData(data)
+      setIsLoading(false)
+    }
+    fetchDashboardData()
+  }, [])
+
+  if (isLoading) {
+    return <h2>Loading...</h2>
+  }
+
+  return (
+    <div>
+      <h2>Dashboard</h2>
+      <h2>Posts - {dashboardData.posts}</h2>
+      <h2>Likes - {dashboardData.likes}</h2>
+      <h2>Followers - {dashboardData.followers}</h2>
+      <h2>Following - {dashboardData.following}</h2>
+    </div>
+  )
+}
+
+export default Dashboard
